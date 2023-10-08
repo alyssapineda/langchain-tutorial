f13aec7099ed358eac0d933d8f94d21b61cabee7
alyssa <110448688+alyssapineda@users.noreply.github.com>
2023-10-08 13:15:24 +1100

    ConversationChain and ConversationBufferMemory

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from secret_key import OPENAI_API_KEY,SERPAPI_API_KEY
 import os
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SimpleSequentialChain, SequentialChain
+from langchain.chains import LLMChain, SimpleSequentialChain, SequentialChain, ConversationChain
 from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.memory import ConversationBufferMemory
 
@@ -20,36 +20,36 @@ llm = OpenAI(temperature = 0.7) #temperature is how 'creative' you want your mod
 
 #---------SIMPLE SEQUENTIAL CHAIN EXAMPLE---------
 #You can use prompt templates to format and pass different cuisines
-prompt_template_name = PromptTemplate(
-  input_variables = ['cuisine'],
-  template = "I want to open a restaurant for {cuisine} food. Suggest a fancy name for this."
-)
+# prompt_template_name = PromptTemplate(
+#   input_variables = ['cuisine'],
+#   template = "I want to open a restaurant for {cuisine} food. Suggest a fancy name for this."
+# )
 
 #prompt_template_name.format(cuisine = "Filipino")
 
 #Chain - makes it easier to pass prompt and input variables
-name_chain = LLMChain(llm=llm, prompt=prompt_template_name)
-name_response = name_chain.run("Filipino")
+#name_chain = LLMChain(llm=llm, prompt=prompt_template_name)
+#name_response = name_chain.run("Filipino")
 #print(name_chain)
 
 
 
 #LET'S TRY AGAIN
-prompt_template_items = PromptTemplate(
-  input_variables = ['restaurant_name'],
-  template = """Suggest some menu items for {restaurant_name}. Return it as a comma separated list."""
-)
+# prompt_template_items = PromptTemplate(
+#   input_variables = ['restaurant_name'],
+#   template = """Suggest some menu items for {restaurant_name}. Return it as a comma separated list."""
+# )
 
-food_items_chain = LLMChain(llm=llm, prompt=prompt_template_items)
+#food_items_chain = LLMChain(llm=llm, prompt=prompt_template_items)
 
-chain = SimpleSequentialChain(
-  chains = [
-    name_chain, 
-    food_items_chain
-  ]
-)
+# chain = SimpleSequentialChain(
+#   chains = [
+#     name_chain, 
+#     food_items_chain
+#   ]
+# )
 
-response = chain.run("Filipino")
+#response = chain.run("Filipino")
 # print(response)
 
 
@@ -59,22 +59,22 @@ prompt_template_name = PromptTemplate(
   template = "I want to open a restaurant for {cuisine} food. Suggest a fancy name for this."
 )
 
-name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key = "restaurant_name")
+# name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key = "restaurant_name")
 
-prompt_template_items = PromptTemplate(
-  input_variables = ['restaurant_name'],
-  template = """Suggest some menu items for {restaurant_name}. Return it as a comma separated list."""
-)
+# prompt_template_items = PromptTemplate(
+#   input_variables = ['restaurant_name'],
+#   template = """Suggest some menu items for {restaurant_name}. Return it as a comma separated list."""
+# )
 
-food_items_chain = LLMChain(llm=llm, prompt=prompt_template_items, output_key="menu_items")
+# food_items_chain = LLMChain(llm=llm, prompt=prompt_template_items, output_key="menu_items")
 
-sequential_chain = SequentialChain(
-  chains = [name_chain, food_items_chain],
-  input_variables = ['cuisine'],
-  output_variables = ['restaurant_name', 'menu_items']
-)
+# sequential_chain = SequentialChain(
+#   chains = [name_chain, food_items_chain],
+#   input_variables = ['cuisine'],
+#   output_variables = ['restaurant_name', 'menu_items']
+# )
 
-sequential_response = sequential_chain({'cuisine':'Filipino'})
+# sequential_response = sequential_chain({'cuisine':'Filipino'})
 #print(sequential_response)
 
 
@@ -95,14 +95,14 @@ sequential_response = sequential_chain({'cuisine':'Filipino'})
 
 
 #__SERPAPI example __
-serp_tools = load_tools(["serpapi", "llm-math"], llm=llm)
+# serp_tools = load_tools(["serpapi", "llm-math"], llm=llm)
 
-agent = initialize_agent(
-  serp_tools,
-  llm,
-  agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-  verbose=True #gives step by step
-)
+# agent = initialize_agent(
+#   serp_tools,
+#   llm,
+#   agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+#   verbose=True #gives step by step
+# )
 
 # serp_agent_response = agent.run("What was the GDP of US in 2022 plus 5?")
 # print(serp_agent_response)
@@ -113,3 +113,13 @@ name_chain = LLMChain(llm=llm, prompt=prompt_template_name, memory=memory)
 name_response = name_chain.run("American")
 print(name_chain)
 #print(chain.memory.buffer)
+
+convo = ConversationChain(llm=OpenAI(temperature=0.7))
+print(convo.prompt.template)
+
+print(convo.run("Who won the first cricket world cup?"))
+print(convo.run("What is 5 + 5?"))
+print(convo.run("Who was the captain of the winning team?"))
+
+#print(convo.memory)
+print(convo.memory.buffer)
