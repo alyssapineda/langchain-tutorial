3abf7c20eb889362d00d33871e2c0a7d15645c37
alyssa <110448688+alyssapineda@users.noreply.github.com>
2023-10-08 11:12:52 +1100

    More examples on simple sequential chain

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from secret_key import OPENAI_API_KEY
 import os
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SimpleSequentialChain
+
 
 # openai.api_key = st.secrets["OPENAI_API_KEY"]
 
@@ -15,7 +16,7 @@ llm = OpenAI(temperature = 0.6) #temperature is how 'creative' you want your mod
 # name = llm("I want to open a restaurant for Indian food. Suggest a fancy name for this")
 # print(name)
 
-#SIMPLE SEQUENTIAL CHAIN EXAMPLE
+#---------SIMPLE SEQUENTIAL CHAIN EXAMPLE---------
 #You can use prompt templates to format and pass different cuisines
 prompt_template_name = PromptTemplate(
   input_variables = ['cuisine'],
@@ -25,9 +26,29 @@ prompt_template_name = PromptTemplate(
 #prompt_template_name.format(cuisine = "Filipino")
 
 #Chain - makes it easier to pass prompt and input variables
-chain = LLMChain(llm=llm, prompt=prompt_template_name)
-print(chain.run("Filipino"))
+name_chain = LLMChain(llm=llm, prompt=prompt_template_name)
+name_response = name_chain.run("Filipino")
+#print(name_chain)
 
 
 
-#Sequential chain
+#LET'S TRY AGAIN
+prompt_template_items = PromptTemplate(
+  input_variables = ['restaurant_name'],
+  template = """Suggest some menu items for {restaurant_name}. Return it as a comma separated list."""
+)
+
+food_items_chain = LLMChain(llm=llm, prompt=prompt_template_items)
+
+chain = SimpleSequentialChain(
+  chains = [
+    name_chain, 
+    food_items_chain
+  ]
+)
+
+response = chain.run("Filipino")
+print(response)
+
+
+#---------SEQUENTIAL CHAIN EXAMPLE---------
